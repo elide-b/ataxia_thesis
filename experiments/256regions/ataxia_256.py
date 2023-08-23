044c789b5708ba4b60a20d92b890701c87a3dc1d
Elide <elide.98@gmail.com>
2023-08-23 12:11:25 +0200

    256 region experiment

---
+++ experiments/256regions/ataxia_256.py
@@ -0,0 +1,19 @@
+import numpy as np
+from skimage.measure import block_reduce
+
+from ataxia.plots import plot_activity
+import plotly.graph_objs as go
+
+data = np.load("global_ataxia.npz")
+
+def reduce(d):
+    return block_reduce(d, block_size=(1, 1), func=np.mean)
+
+plot_activity(
+    {
+        "Healthy": reduce(data["tavg"][1, 1]),
+        "Global ataxia (All)": reduce(data["tavg"][0, 0]),
+        "Global ataxia (W)": reduce(data["tavg"][0, 1]),
+        "Global ataxia (weights)": reduce(data["tavg"][1, 0]),
+    }
+).write_html("global_ataxia.html")
