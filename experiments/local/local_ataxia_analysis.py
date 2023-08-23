765b88399e262a547ff941a6f1ffffa189b6caef
Robin De Schepper <robin.deschepper93@gmail.com>
2023-08-23 10:58:52 +0200

    fixed local ataxia copy pasta

--- experiments/local/local_ataxia_analysis.py
+++ experiments/local/local_ataxia_analysis.py
@@ -2,13 +2,13 @@ import numpy as np
 
 from ataxia.plots import plot_activity
 
-data = np.load("global_ataxia.npz")
+data = np.load("local_ataxia.npz")
 
 plot_activity(
     {
         "Healthy": data["tavg"][2, 2],
-        "Global ataxia (All)": data["tavg"][0, 0],
-        "Global ataxia (W)": data["tavg"][0, 2],
-        "Global ataxia (weights)": data["tavg"][2, 0],
+        "Cerebellar ataxia (All)": data["tavg"][0, 0],
+        "Cerebellar ataxia (W)": data["tavg"][0, 2],
+        "Cerebellar ataxia (weights)": data["tavg"][2, 0],
     }
-).write_html("global_ataxia.html")
+).write_html("local_ataxia.html")
