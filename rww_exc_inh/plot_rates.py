21a2c354c1971e4befe5b0b9a72580b4b050a9ea
Elide <111415334+elide-b@users.noreply.github.com>
2023-11-02 20:43:21 +0100

    rww_models

---
+++ rww_exc_inh/plot_rates.py
@@ -0,0 +1,28 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+with np.load("firing_rate.npz") as rates:
+    t = rates['time']
+    data = rates['tavg']
+
+S_e = data[:, 0, :, 0]
+S_i = data[:, 1, :, 0]
+R_e = data[:, 2, :, 0]
+R_i = data[:, 3, :, 0]
+
+plt.figure()
+plt.plot(t, S_e, color='r', label='S_E')
+plt.plot(t, S_i, color='b', label='S_I')
+plt.title('Synaptic gating variables')
+plt.legend()
+plt.show()
+
+plt.figure()
+plt.plot(t, R_e, color='r', label='r_E')
+# plt.plot(t, R_i, color='b', label='r_I')
+plt.title('Firing rates')
+plt.legend()
+plt.show()
+
+print(np.mean(np.mean(R_e[100:,:],axis=1)))
+print(np.mean(np.mean(R_i[100:,:],axis=1)))
