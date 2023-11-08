7a0151937f2f936c46eb553ade878028a618f798
Elide <elide.98@gmail.com>
2023-11-08 16:13:28 +0100

    added zmin and zmax in function

--- plots.py
+++ plots.py
@@ -10,8 +10,14 @@ from plotly.subplots import make_subplots
 pio.templates.default = pio.templates["simple_white"]
 
 
-def _add_matrix_panel(fig, matrix, row=None, col=None):
-    fig.add_trace(go.Heatmap(z=matrix), row=row, col=col)
+def _add_matrix_panel(fig, matrix, row=None, col=None, mask_from=None, mask_to=None, zmin=None, zmax=None):
+    if mask_from is not None:
+        sx, sy = get_submask(mask_from, mask_to)
+        matrix = apply_submask(matrix, sx, sy)
+        text = np.array([f"({x}, {y})" for x, y in zip(sx.flatten(), sy.flatten())]).reshape(matrix.shape)
+    else:
+        text = None
+    fig.add_trace(go.Heatmap(z=matrix, zmin=zmin, zmax=zmax, text=text), row=row, col=col)
     fig.update_yaxes(scaleanchor="x", row=row, col=col)
 
 
@@ -20,12 +26,12 @@ def _add_weights_panel(fig, conn, row=None, col=None):
 
 
 def _add_act_panel(fig, activity, row, col):
-    fig.add_trace(go.Heatmap(z=activity, zmin=0, zmax=1), row=row, col=col)
+    fig.add_trace(go.Heatmap(z=activity), row=row, col=col)
 
 
-def plot_matrix(matrix, show=True):
+def plot_matrix(matrix, show=True, mask_from=None, mask_to=None, zmin=None, zmax=None):
     fig = go.Figure()
-    _add_matrix_panel(fig, matrix)
+    _add_matrix_panel(fig, matrix, mask_from=mask_from, mask_to=mask_to, zmin=zmin, zmax=zmax)
     if show:
         fig.show()
     return fig
@@ -51,11 +57,11 @@ def _plot_dict_grid(data: dict[str, Any], grid_panel):
     return fig
 
 
-def plot_weights(conn: Union[Connectivity, dict[str, Connectivity]], show=True):
+def plot_weights(conn: Union[Connectivity, dict[str, Connectivity]], show=True, mask_from=None, mask_to=None, zmin=None, zmax=None):
     if isinstance(conn, dict):
         fig = _plot_dict_grid(conn, _add_weights_panel)
     else:
-        fig = plot_matrix(conn.weights, show=False)
+        fig = plot_matrix(conn.weights, show=False, mask_from=mask_from, mask_to=mask_to, zmin=zmin, zmax=zmax)
     if show:
         fig.show()
     return fig
@@ -69,3 +75,13 @@ def plot_activity(act: Union[np.ndarray, dict[str, np.ndarray]], show=True):
     if show:
         fig.show()
     return fig
+
+
+def get_submask(x, y=None):
+    if y is None:
+        y = x
+    return np.meshgrid(x, y)
+
+
+def apply_submask(m, x, y):
+    return m[x, y].T
