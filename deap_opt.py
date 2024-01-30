3520e3d7b8078833811dc11db6ece513458f79b3
Robin De Schepper <robin.deschepper93@gmail.com>
2024-01-30 22:25:29 +0100

    new deap

--- deap_opt.py
+++ deap_opt.py
@@ -3,17 +3,23 @@ import numpy as np
 from connectivity import load_mousebrain
 from simulation import simulate_modified
 
-from mpipool import MPIExecutor
-
 from super_awesome_deap.optimization import optimize
+from super_awesome_deap.tools import Individual, Param
 
 brain = load_mousebrain("Connectivity_596.h5", norm="log", scale="region")
 
-def evaluate(individual):
+
+class Mousebrain(Individual):
+    w = Param(0, 2)
+    Ji = Param(0.001, 2)
+    G = Param(0, 10)
+
+
+def evaluate(mousebrain):
     (t1, tavg), (t2, bold) = simulate_modified(
-        brain, individual.G, individual.Ji, individual.w, T=2
+        brain, mousebrain.G, mousebrain.Ji, mousebrain.w, T=2
     )
-    print("EVAL", float(np.average(tavg)))
     return (float(np.average(tavg)),)
 
-optimize(evaluate)
+
+optimize(Mousebrain, evaluate)
